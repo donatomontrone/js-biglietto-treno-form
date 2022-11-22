231d18f93c633163956c92d186792484c60566ad
donatomontrone <donato.montrone@outlook.com>
2022-11-22 17:24:36 +0100

    add print in console

--- js/script.js
+++ js/script.js
@@ -16,17 +16,19 @@ const button = document.querySelector('button');
 button.addEventListener('click', function() {
     //Prezzo del biglietto standard
     let ticketPrice = (parseInt(inputKmetres.value)) * 0.2762; 
-    console.log(ticketPrice)
 
     if ((parseInt(inputUserAge.value) > 0) && (parseInt(inputUserAge.value) < 120)){
         if (parseInt(inputUserAge.value) < 18){
             ticketPrice = ticketPrice * 0.825;
             priceResult.innerHTML = `i km che vorresti percorrere sono ${inputKmetres.value}km e considerando l'età (${inputUserAge.value}), questo è il prezzo del biglietto scontato under 18: ${ticketPrice.toFixed(2)}&euro;`;
+            console.log(ticketPrice.toFixed(2))
         } else if (parseInt(inputUserAge.value) >= 65){
             ticketPrice = ticketPrice * 0.667;
             priceResult.innerHTML = `I km che vorresti percorrere sono ${inputKmetres.value}km e considerando l'età (${inputUserAge.value}), questo è il prezzo del biglietto scontato over 65: ${ticketPrice.toFixed(2)}&euro;`;
+            console.log(ticketPrice.toFixed(2))
         } else {
             priceResult.innerHTML = `I km che vorresti percorrere sono ${inputKmetres.value}km e questo è il prezzo del biglietto: ${ticketPrice.toFixed(2)}&euro;`;
+            console.log(ticketPrice.toFixed(2))
         }
     } else {
         alert('L\'età inserita non è corretta')
